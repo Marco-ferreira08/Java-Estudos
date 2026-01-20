5c91e25907ee713bfbcd1987148bfb86a2b33139
Marco Antônio Ferreira <ferreiramarcoantonio904@gmail.com>
2026-01-20 17:34:37 -0300

    Add files via upload

---
+++ src/academy/devdojo/maratonajava/introducion/Operadores.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducion;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        // + - / *
+
+        int numero1 = 10;
+        int numero2 = 20;
+
+        System.out.println(numero1 + numero2);
+        System.out.println(numero2 - numero1);
+        System.out.println(numero1 * numero2);
+        System.out.println(numero2 / numero1);
+
+
+    }
+}
